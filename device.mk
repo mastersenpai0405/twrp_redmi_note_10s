6f8885141d46d3b2499873b10637d4cf7015913f
mastersenpai05 <pampangavergil0405@gmail.com>
2021-08-23 20:53:24 +0800

    ares: nuke

--- device.mk
+++ device.mk
@@ -20,7 +20,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-service \
-    android.hardwate.boot@1.0-impl
     android.hardware.boot@1.0-impl-1.1-mtkimpl.so
 
 PRODUCT_PACKAGES += \
